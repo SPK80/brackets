473927a74e04a2068f2926a446a01271559e436c
SPK80 <psukonkin@gmail.com>
2022-01-28 15:53:49 +0300

    feat: add isUnary and change isOpening

--- mytest.js
+++ mytest.js
@@ -1,9 +1,10 @@
 const check = require('./src/index.js');
 const config2 = [['(', ')'], ['[', ']']];
 const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
 
 console.log(check(
-	'||', config4
+	'|()|(||)||', config5
 ));
 
 // console.log(check('((()))()', [['(', ')']]));
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-function isOpening(bracket, bracketsGroup) {
-	return bracket == bracketsGroup[0]
+function isOpening(bracket, bracketsGroup, stack, index) {
+	if (isUnary(bracketsGroup))
+		return (stack.length == 0 || stack[stack.length - 1] != index);
+	else
+		return bracket == bracketsGroup[0];
 }
 
 function bracketsIndex(bracket, bracketsConfig) {
 	return bracketsConfig.findIndex(bc => (bc[0] === bracket || bc[1] === bracket))
 }
 
+function isUnary(bracketsGroup) {
+	return bracketsGroup[0] === bracketsGroup[1];
+}
+
 module.exports = function check(str, bracketsConfig) {
 	const stack = [];
 	const result = [...str].find(bracket => {
 		const index = bracketsIndex(bracket, bracketsConfig);
-		if (isOpening(bracket, bracketsConfig[index])) {
+		if (isOpening(bracket, bracketsConfig[index], stack, index)) {
 			stack.push(index);
 			return false;
 		}
